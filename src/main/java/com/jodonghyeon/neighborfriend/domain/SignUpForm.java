0db7f9b87818a06cf9b9e3ac9968715e99785d7d
jodonghyeon <jawoo1003@gmail.com>
2023-07-21 19:23:23 +0900

    [001-SIGN-UP_유저회원가입] 기본 유저 회원가입 완료

---
+++ src/main/java/com/jodonghyeon/neighborfriend/domain/SignUpForm.java
@@ -0,0 +1,17 @@
+package com.jodonghyeon.neighborfriend.domain;
+
+import com.jodonghyeon.neighborfriend.domain.Type.Gender;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class SignUpForm {
+    private String email;
+    private String name;
+    private String password;
+    private LocalDate birth;
+    private String phone;
+    private Gender gender;
+    private Long age;
+}
---
+++ src/main/java/com/jodonghyeon/neighborfriend/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jodonghyeon.neighborfriend.domain.repository;
+
+import com.jodonghyeon.neighborfriend.domain.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}
