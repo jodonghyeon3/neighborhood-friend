388e1a5d32e2767322ff58499ad2e53216901617
jodonghyeon <jawoo1003@gmail.com>
2023-07-21 16:02:48 +0900

    Initial commit

---
+++ src/main/java/com/jodonghyeon/neighborfriend/NeighborFriendApplication.java
@@ -0,0 +1,15 @@
+package com.jodonghyeon.neighborfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+
+@SpringBootApplication
+public class NeighborFriendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeighborFriendApplication.class, args);
+
+    }
+}
---
+++ src/test/java/com/jodonghyeon/neighborfriend/NeighborFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jodonghyeon.neighborfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborFriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
