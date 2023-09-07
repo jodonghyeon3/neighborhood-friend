a55c647a30a14f766cf33e010430aa2c2742db43
jodonghyeon3 <117457834+jodonghyeon3@users.noreply.github.com>
2023-09-07 21:34:59 +0900

    Update and rename scripts/deploy.sh to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+!/bin/bash
+/home/ec2-user/action/gradlew build
 BUILD_JAR=$(ls /home/ec2-user/action/build/libs/neighbor-friend.jar)
 JAR_NAME=neighbor-friend.jar
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
